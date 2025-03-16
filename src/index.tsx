1019a1c100b1313c4a8517468d748d6bd79d76b0
Daniele <daniele@anothertomorrow.io>
2025-03-16 16:17:18 +0100

    Downgrade to React 17 to resolve Needle compatibility issues with react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
 /* Import Neo4j Design Language base style */
 import '@neo4j-ndl/base/lib/neo4j-ds-styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
