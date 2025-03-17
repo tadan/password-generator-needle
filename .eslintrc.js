3766459cde51d6dd4659db5fd275d62ac0d86beb
Daniele <daniele@anothertomorrow.io>
2025-03-17 22:53:32 +0100

    chore: Update ESLint config and dependencies for code quality
    - Add custom ESLint rules for consistent code style
    - Configure TypeScript-specific linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-    ],
-    rules: {
-        'react/prop-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
-    },
-}
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
